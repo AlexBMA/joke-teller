104f28481998503a0bace59ba4355785a3e2c3be
Alexandru Burghelea <burghelea.alex@gmail.com>
2020-08-24 12:12:12 +0300

    update the html and js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-import {apikey} from './config.js';
+import { apikey } from './config.js';
 
 // VoiceRSS Javascript SDK
 const VoiceRSS = {
@@ -106,23 +106,41 @@ const VoiceRSS = {
   },
 };
 
-
 const APIKEY = apikey;
 
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audioElement');
 
-function test() {
-  VoiceRSS.speech({
-    key: APIKEY,
-    src: 'Hello, world!',
-    hl: 'en-us',
-    v: 'Linda',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  });
+const apiUrl =
+  'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist&type=single';
+
+async function getJokes() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const joke = data.joke;
+    button.hidden = true;
+
+    VoiceRSS.speech({
+      key: APIKEY,
+      src: joke,
+      hl: 'en-us',
+      v: 'Linda',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false,
+    });
+  } catch (error) {
+    console.log('Whoops', error);
+  }
 }
 
-test();
+
+function toogleButton(){
+    button.hidden = false;
+}
+
+button.addEventListener('click',getJokes);
+audioElement.addEventListener('ended',toogleButton);
+
